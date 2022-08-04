da391041a2356be80e267b4e97a8ac9628e01460
Radu Dita <hades2510@yahoo.com>
2022-08-04 23:32:09 +0300

    add more tests for computeBoardPosition

--- src/utils/game.spec.js
+++ src/utils/game.spec.js
@@ -7,5 +7,15 @@ describe("Game utils methods", () => {
 
       expect(pos).toBe(0);
     });
+    it("should be 4", () => {
+        const pos = computeBoardPosition(1, 1);
+
+        expect(pos).toBe(4);
+    })
+    it("returns NaN", () => {
+        const pos = computeBoardPosition(NaN, 0);
+
+        expect(pos).toBeNaN();
+    })
   });
 });
