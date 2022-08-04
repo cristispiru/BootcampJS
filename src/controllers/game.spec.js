acccf611cb605ccf728bf0cdd81326f17e13e1be
Radu Dita <hades2510@yahoo.com>
2022-08-05 00:27:25 +0300

    add specs for failuer

--- src/controllers/game.spec.js
+++ src/controllers/game.spec.js
@@ -1,5 +1,9 @@
 import gameController from "./game";
 
+jest.mock("../services/game.js", () => ({
+    getAll: () => [2]
+}))
+
 describe("Game Controller", () => {
   it("should create a game", async () => {
     const json = jest.fn();
@@ -14,5 +18,17 @@ describe("Game Controller", () => {
 
     expect(json).toBeCalled();
     expect(Array.isArray(json.mock.calls[0][0])).toBe(true)
+  });
+
+  it("should throw an error", async () => {
+    const next = jest.fn();
+
+    await gameController.getGames(
+      {},
+      {},
+      next
+    );
+
+    expect(next).toBeCalled();
   });
 });
