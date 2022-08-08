ef69113e45baecc064e01900da5acd4ff10ac46e
Cristi Spiru <cristispiru@yahoo.com>
2022-08-09 00:24:05 +0300

    Added token decode & validation middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,26 @@
+export const jwtMiddleware = async (req, res, next) => {
+    if (!process.env.JWT_SECRET) {
+        throw new Error('Missing JWT_SECRET env var')
+    }
+
+    const authHeader = req.headers.authorization
+
+    if (!authHeader) {
+        res.status(403).send("Unauthorized")
+        return
+    }
+
+    const token = authHeader.split('Bearer ')[1]
+    verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+        try {
+            if (err) {
+                res.status(403).send("Invalid token");
+            } else {
+                req.auth = decoded;
+                next()
+            }
+        } catch (e) {
+            res.status(422).send("Invalid token");
+        }
+    })
+}
